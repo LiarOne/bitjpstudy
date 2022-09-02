465e66df8f5b562d463b3ebf799ca54f06bcc34d
LiarOne <853540131@qq.com>
2022-09-03 00:00:02 +0900

    sql

---
+++ sql/中等/184. 部门工资最高的员工.sql
@@ -0,0 +1,8 @@
+select Department.name as Department, Employee.name as Employee, Salary
+from Employee join Department on Employee.DepartmentId = Department.Id
+where (Employee.DepartmentId, Salary) in 
+(
+    select DepartmentId, max(Salary)
+    from Employee
+    group by DepartmentId
+)
