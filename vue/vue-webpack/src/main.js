d8fd2b7999f71c0cabc7c59f425db4f8cd304d20
LiarOne <853540131@qq.com>
2022-09-29 14:42:02 +0900

    empty page no bug

--- vue/vue-webpack/src/main.js
+++ vue/vue-webpack/src/main.js
@@ -1,1 +1,11 @@
-console.log('ok')
+// import Vue from 'vue'
+
+
+// const login = {
+//     template:'<h3>这是Login组件</h3>'
+// }
+
+// const vm = new Vue({
+//     el:'#app',
+//     render: c=>c(login)
+// })
