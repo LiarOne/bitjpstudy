1c6d69457abdf16406a1245396ef47d9b1371b10
LiarOne <853540131@qq.com>
2022-09-29 14:46:17 +0900

    bug1 appear

--- vue/vue-webpack/src/main.js
+++ vue/vue-webpack/src/main.js
@@ -1,11 +1,11 @@
-// import Vue from 'vue'
+import Vue from 'vue'
 
 
-// const login = {
-//     template:'<h3>这是Login组件</h3>'
-// }
+const login = {
+    template:'<h3>这是Login组件</h3>'
+}
 
-// const vm = new Vue({
-//     el:'#app',
-//     render: c=>c(login)
-// })
+const vm = new Vue({
+    el:'#app',
+    render: c=>c(login)
+})
