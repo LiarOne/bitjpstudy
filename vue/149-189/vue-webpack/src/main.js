ce50ce741e1dd61f62521fc4166323a52ab18142
LiarOne <853540131@qq.com>
2022-09-30 22:10:55 +0900

    router module

--- vue/149-189/vue-webpack/src/main.js
+++ vue/149-189/vue-webpack/src/main.js
@@ -4,15 +4,7 @@ import app from './components/App.vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
-import Login from './components/account/Login.vue'
-import Reg from './components/account/Reg.vue'
-
-const router = new VueRouter({
-    routes: [
-        { path: '/login', component: Login },
-        { path: '/reg', component: Reg }
-    ]
-})
+import router from './router.js'
 
 const vm = new Vue({
     el:'#app',
