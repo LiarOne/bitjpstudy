3c366409c0afe505f6cde816d7d880dd60e5265f
LiarOne <853540131@qq.com>
2022-10-01 21:01:39 +0900

    tabbar router

--- vue/149-189/vue-cms/src/components/App.vue
+++ vue/149-189/vue-cms/src/components/App.vue
@@ -2,25 +2,27 @@
     <div class="app-container">
         <!--header区域-->
         <mt-header fixed title="固定在顶部"></mt-header>
-        <h3>这是App根组件</h3>
+
+        <router-view></router-view>
+        
         <!--tabbar-->
         <nav class="mui-bar mui-bar-tab">
-			<a class="mui-tab-item mui-active" href="#tabbar">
+			<router-link class="mui-tab-item mui-active" to="/home">
 				<span class="mui-icon mui-icon-home"></span>
 				<span class="mui-tab-label">首页</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-chat">
+            </router-link>
+			<router-link class="mui-tab-item" to="/member">
 				<span class="mui-icon mui-icon-contact"></span>
 				<span class="mui-tab-label">会员</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-contact">
+            </router-link>
+			<router-link class="mui-tab-item" to="/shopcart">
 				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">0</span></span>
 				<span class="mui-tab-label">购物车</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-map">
+            </router-link>
+			<router-link class="mui-tab-item" to="/search">
 				<span class="mui-icon mui-icon-search"></span>
 				<span class="mui-tab-label">搜索</span>
-			</a>
+            </router-link>
 		</nav>
     </div>
 </template>
--- vue/149-189/vue-cms/src/main.js
+++ vue/149-189/vue-cms/src/main.js
@@ -8,7 +8,25 @@ Vue.use(MintUI)
 import '../lib/mui/css/mui.min.css'
 import '../lib/mui/css/icons-extra.css'
 
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import HomeContainer from './components/tabbars/HomeContainer.vue'
+import MemberContainer from './components/tabbars/MemberContainer.vue'
+import ShopcartContainer from './components/tabbars/ShopcartContainer.vue'
+import SearchContainer from './components/tabbars/SearchContainer.vue'
+
+const router = new VueRouter({
+    routes: [
+        { path: '/home', component: HomeContainer },
+        { path: '/member', component: MemberContainer },
+        { path: '/shopcart', component: ShopcartContainer },
+        { path: '/search', component: SearchContainer },
+    ]
+})
+
 const vm = new Vue({
     el: '#app',
-    render: c=>c(App)
+    render: c=>c(App),
+    router
 })
