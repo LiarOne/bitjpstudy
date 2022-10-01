d2e47518ed6a212b5192b8717f50fcd2290c47ff
LiarOne <853540131@qq.com>
2022-10-01 20:46:13 +0900

    tabbar

--- vue/149-189/vue-cms/src/components/App.vue
+++ vue/149-189/vue-cms/src/components/App.vue
@@ -1,7 +1,27 @@
 <template>
     <div class="app-container">
+        <!--header区域-->
         <mt-header fixed title="固定在顶部"></mt-header>
         <h3>这是App根组件</h3>
+        <!--tabbar-->
+        <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">会员</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">0</span></span>
+				<span class="mui-tab-label">购物车</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-search"></span>
+				<span class="mui-tab-label">搜索</span>
+			</a>
+		</nav>
     </div>
 </template>
 
--- vue/149-189/vue-cms/src/main.js
+++ vue/149-189/vue-cms/src/main.js
@@ -5,6 +5,9 @@ import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 Vue.use(MintUI)
 
+import '../lib/mui/css/mui.min.css'
+import '../lib/mui/css/icons-extra.css'
+
 const vm = new Vue({
     el: '#app',
     render: c=>c(App)
