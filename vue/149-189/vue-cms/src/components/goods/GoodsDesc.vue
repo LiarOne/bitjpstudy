bf4c4f16b0d59aa5d2c64e601a1fc5af6a4a179e
LiarOne <853540131@qq.com>
2022-10-10 21:27:39 +0900

    goodscomment

--- vue/149-189/vue-cms/src/components/goods/GoodsDesc.vue
+++ vue/149-189/vue-cms/src/components/goods/GoodsDesc.vue
@@ -1,18 +1,45 @@
 <template>
-    <div>
-        <h3>商品描述页面</h3>
+    <div class="goodsdesc-container">
+        <h3 class="title">{{info.title}}</h3>
+        <hr>
+        <div class="content" v-html="info.content"></div>
     </div>
 </template>
 
 <script>
 export default {
     data(){
-        return {}
+        return {
+            info:{}
+        }
     },
-    methods: {}
+    created(){
+        this.getGoodsDesc();
+    },
+    methods: {
+        async getGoodsDesc(){
+            const {data} = await this.$http.get('/api/goods/getdesc/'+this.id);
+            if(data.status===0) return (this.info=data.message[0]);
+        }
+    },
+    props: ["id"]
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.goodsdesc-container{
+    
+.title{
+    font-size: 16px;
+    margin: 10px 0;
+    color: blue;
+    text-align: center;
+}
+.content{
+    img{
+        width: 100%;
+    }
+}
 
+}
 </style>
